142f512302a2f9925e184cd0da8922e3b2ac443a
brosula <brosula@broadinstitute.org>
2024-08-20 11:46:53 -0400

    Remove packages not necessary for ASV filtering

--- Code/MHap/amplseq_required_libraries.R
+++ Code/MHap/amplseq_required_libraries.R
@@ -1,16 +1,16 @@
 
-if(!require(adegenet)){
-  options(
-    repos = c(
-      zkamvar = "https://zkamvar.r-universe.dev",
-      CRAN = "https://cloud.r-project.org"
-    )
-  )
-  install.packages("adegenet")
-  library(adegenet)
-}else{
-  library(adegenet)
-}
+# if(!require(adegenet)){
+#   options(
+#     repos = c(
+#       zkamvar = "https://zkamvar.r-universe.dev",
+#       CRAN = "https://cloud.r-project.org"
+#     )
+#   )
+#   install.packages("adegenet")
+#   library(adegenet)
+# }else{
+#   library(adegenet)
+# }
 
 if(!require(ade4)){
   install.packages("ade4")
@@ -19,12 +19,12 @@ if(!require(ade4)){
   library(ade4)
 }
 
-if(!require(poppr)){
-  install.packages("poppr")
-  library(poppr)
-}else{
-  library(poppr)
-}
+# if(!require(poppr)){
+#   install.packages("poppr")
+#   library(poppr)
+# }else{
+#   library(poppr)
+# }
 
 if(!require(dplyr)){
   install.packages("dplyr")
@@ -54,12 +54,12 @@ if(!require(ggplot2)){
   library(ggplot2)
 }
 
-if(!require(cowplot)){
-  install.packages("cowplot")
-  library(cowplot)
-}else{
-  library(cowplot)
-}
+# if(!require(cowplot)){
+#   install.packages("cowplot")
+#   library(cowplot)
+# }else{
+#   library(cowplot)
+# }
 
 
 if(!require(vegan)){
@@ -69,12 +69,12 @@ if(!require(vegan)){
   library(vegan)
 }
 
-if(!require(parallel)){
-  install.packages("parallel")
-  library(parallel)
-}else{
-  library(parallel)
-}
+# if(!require(parallel)){
+#   install.packages("parallel")
+#   library(parallel)
+# }else{
+#   library(parallel)
+# }
 
 if(!require(ape)){
   install.packages("ape")
@@ -83,47 +83,47 @@ if(!require(ape)){
   library(ape)
 }
 
-if(!require(pegas)){
-  install.packages("pegas")
-  library(pegas)
-}else{
-  library(pegas)
-}
+# if(!require(pegas)){
+#   install.packages("pegas")
+#   library(pegas)
+# }else{
+#   library(pegas)
+# }
 
-if(!require(RColorBrewer)){
-  install.packages("RColorBrewer")
-  library(RColorBrewer)
-}else{
-  library(RColorBrewer)
-}
+# if(!require(RColorBrewer)){
+#   install.packages("RColorBrewer")
+#   library(RColorBrewer)
+# }else{
+#   library(RColorBrewer)
+# }
 
-if(!require(Hmisc)){
-  install.packages('Hmisc')
-  library(Hmisc)
-}else{
-  library(Hmisc)
-}
+# if(!require(Hmisc)){
+#   install.packages('Hmisc')
+#   library(Hmisc)
+# }else{
+#   library(Hmisc)
+# }
 
-if(!require(ggpubr)){
-  install.packages('ggpubr')
-  library(ggpubr)
-}else{
-  library(ggpubr)
-}
+# if(!require(ggpubr)){
+#   install.packages('ggpubr')
+#   library(ggpubr)
+# }else{
+#   library(ggpubr)
+# }
 
-if(!require(doMC)){
-  install.packages('doMC')
-  library(doMC)
-}else{
-  library(doMC)
-}
+# if(!require(doMC)){
+#   install.packages('doMC')
+#   library(doMC)
+# }else{
+#   library(doMC)
+# }
 
-if(!require(svMisc)){
-  install.packages('svMisc')
-  library(svMisc)
-}else{
-  library(svMisc)
-}
+# if(!require(svMisc)){
+#   install.packages('svMisc')
+#   library(svMisc)
+# }else{
+#   library(svMisc)
+# }
 
 if(!require(Biostrings)){
   if (!require("BiocManager", quietly = TRUE)){
@@ -135,19 +135,19 @@ if(!require(Biostrings)){
   library(Biostrings)
 }
 
-if(!require(Rcpp)){
-  install.packages('Rcpp')
-  library(Rcpp)
-}else{
-  library(Rcpp)
-}
+# if(!require(Rcpp)){
+#   install.packages('Rcpp')
+#   library(Rcpp)
+# }else{
+#   library(Rcpp)
+# }
 
-if(!require(DT)){
-  install.packages('DT')
-  library(DT)
-}else{
-  library(DT)
-}
+# if(!require(DT)){
+#   install.packages('DT')
+#   library(DT)
+# }else{
+#   library(DT)
+# }
 
 if(!require(stringr)){
   install.packages('stringr')
@@ -156,17 +156,15 @@ if(!require(stringr)){
   library(stringr)
 }
 
-if(!require(XLConnect)){
-  install.packages('XLConnect')
-  library(XLConnect)
-}
+# if(!require(XLConnect)){
+#   install.packages('XLConnect')
+#   library(XLConnect)
+# }
 
-
-if(!require(tmap)){
-  install.packages('tmap')
-  library(tmap)
-}
-
+# if(!require(tmap)){
+#   install.packages('tmap')
+#   library(tmap)
+# }
 
 if(!require(sp)){
   install.packages('sp')
